3d6b071010fc807183238707170a6533c0d9c71f
Michel SADEU <michelsadeungakou@gmail.com>
2023-08-10 17:38:49 +0200

    Validation d'un message de print

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ import java.io.*;
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World, comment allez vous ?");
+        System.out.println("Je vais super bien merci !");
     }
 }
