0e25ddece4c1f426bc9020e82cd17f31a5cd6584
Michel SADEU <michelsadeungakou@gmail.com>
2024-12-08 02:32:51 +0100

    Update message

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Hello World, comment allez vous ?");
         System.out.println("Je vais super bien merci !");
-        System.out.println("Je compte voyager demain matin, merci d'y penser");
+        System.out.println("Je compte sur vous");
         
     }
 }
