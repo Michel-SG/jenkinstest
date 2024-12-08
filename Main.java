7fbd8e18e5f4191e90c20b78b0e54f4a850cfa83
Michel SADEU <michelsadeungakou@gmail.com>
2024-12-08 02:30:35 +0100

    appfiles/dd message

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World, comment allez vous ?");
-        System.out.println("Je vais super bien merci !");
+        System.out.println("Je vais super bien merci et toi ça va ?");
        
         
     }
